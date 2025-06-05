e7c7b01b0cef5c2103af631c1695f2fb032d45a6
vguzman812 <vguzman812@gmail.com>
2025-06-05 05:33:24 -0400

    typescript configured and able to run files via tsx

--- backend/index.ts
+++ backend/index.ts
@@ -1,1 +1,14 @@
-console.log("hello world");
+import http from "http";
+
+const PORT = 3001;
+
+const app = http.createServer((request, response) => {
+    const url = request.headers;
+    response.writeHead(200, { "Content-Type": "text/plain" });
+    response.write(`Hello, World! The headers are: ${JSON.stringify(url)}`);
+    response.end();
+});
+
+app.listen(PORT, () => {
+    console.log("Server running on port 3001");
+});
